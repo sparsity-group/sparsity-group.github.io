5c2105c603d81f02978d1dd220a7fdc63f2bdfe5
Rahul Parhi <rahul@parhi.com>
2017-08-19 21:43:20 -0500

    Update deploy script to pull in CV from own repo

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,42 @@
 #!/bin/sh
 
-cd _site
+# grabs most recent cv from GitHub
+make_cv() {
+    mkdir -p files
 
-git init
-git remote add origin https://github.com/rp/rp.github.io.git
-git add .
-git commit -m "deploy"
-git push --set-upstream origin master --force
-rm -rf .git
+    WORKING_DIR=$(pwd)
+    CV_DIR=$(mktemp -d)
+
+    git clone https://github.com/rp/rahul-cv.git $CV_DIR
+    cd $CV_DIR
+
+    make
+
+    mv *.pdf "$WORKING_DIR/files"
+
+    cd $WORKING_DIR
+    rm -rf $CV_DIR
+}
+
+# pushes site to GitHub
+push() {
+    git init
+    git remote add origin https://github.com/rp/rp.github.io.git
+    git add .
+    git commit --allow-empty-message -m ""
+    git push --set-upstream origin master --force
+    rm -rf .git
+}
+
+deploy() {
+    cd _site
+    make_cv
+    push
+}
+
+if [ -d "$DIRECTORY" ]; then
+    deploy
+else
+    stack exec site build
+    deploy
+fi
